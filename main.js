ad0c689677481ad3891691495365e8714047792b
PedroHenriqueDEV26 <112581681+PedroHenriqueDEV26@users.noreply.github.com>
2024-06-04 14:43:08 -0300

    Add files via upload
    aula 128

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var music1 = ""
-
+var pulsoEsqX = 0;
+var pulsoEsqY = 0;
+var pulsoDirX = 0;
+var pulsoDirY = 0;
 
 function preload(){
     music1 = loadSound("musicID.mp3");
@@ -10,13 +13,37 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on("pose", gotPoses)
+}
+
+function modelLoaded(){
+console.log("modelo carregado")
 }
 
 function draw(){
     image(video,0,0,450,320);
+    fill("blue")
+    circle(pulsoDirX,pulsoDirY, 30)
+    fill("red")
+    circle(pulsoEsqX,pulsoEsqY,30)
     
 }
 
 function playM(){
     music1.play();
+    music1.setVolume(0.5)
+    music1.rate(1)
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results)
+
+        pulsoDirX = results[0].pose.rightWrist.x
+        pulsoDirY = results[0].pose.rightWrist.y
+
+        pulsoEsqX = results[0].pose.leftWrist.x
+        pulsoEsqY = results[0].pose.leftWrist.y
+    }
 }
